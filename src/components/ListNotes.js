1ada8c42e90f1604c14b6f82c3da4d49748333bd
Aristotel1990 <aristotel.dhrako@gmail.com>
2022-11-04 16:11:59 +0100

    third commit

--- src/components/ListNotes.js
+++ src/components/ListNotes.js
@@ -13,45 +13,44 @@ import {
 export default function ListNotes({ notes = [] }) {
   return (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-      {notes.length !== 0
-        ? notes.map((row, index) => {
-            return (
-              <>
-                <ListItem key={row.id} alignItems="flex-start">
-                  <ListItemAvatar>
-                    <Avatar
-                      alt="Travis Howard"
-                      src="/static/images/avatar/2.jpg"
-                    />
-                  </ListItemAvatar>
-                  <ListItemText
-                    key={row.id}
-                    primary={row?.title}
-                    secondary={
-                      <>
-                        <Typography
-                          sx={{ display: "inline" }}
-                          component="span"
-                          variant="body2"
-                          color="text.primary"
-                        ></Typography>
-                        {row?.note}
-                        <br />
-                        Date : {dayjs(row?.date).format("YYYY-MM-DD -  HH:mm")}
-                      </>
-                    }
+      {notes &&
+        notes.map((row, index) => {
+          return (
+            <>
+              <ListItem key={row.id} alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar
+                    alt="Travis Howard"
+                    src="/static/images/avatar/2.jpg"
                   />
-                  <Chip
-                    color="primary"
-                    variant="outlined"
-                    label={row?.important}
-                  />
-                </ListItem>
-                <Divider variant="inset" component="li" />
-              </>
-            );
-          })
-        : ""}
+                </ListItemAvatar>
+                <ListItemText
+                  key={row.id}
+                  primary={row?.title}
+                  secondary={
+                    <>
+                      <Typography
+                        sx={{ display: "inline" }}
+                        component="span"
+                        variant="body2"
+                        color="text.primary"
+                      ></Typography>
+                      {row?.note}
+                      <br />
+                      Date : {dayjs(row?.date).format("YYYY-MM-DD -  HH:mm")}
+                    </>
+                  }
+                />
+                <Chip
+                  color="primary"
+                  variant="outlined"
+                  label={row?.important}
+                />
+              </ListItem>
+              <Divider variant="inset" component="li" />
+            </>
+          );
+        })}
     </List>
   );
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { Typography, Stack } from "@mui/material";
 import { useDispatch } from "../redux/store";
 import { getNotesFromStorage } from "../redux/slices/notes";
+import { Grid } from "@mui/material";
 
 // ----------------------------------------------------------------------
 
@@ -13,20 +14,22 @@ export default function Welcome() {
   }, []);
 
   return (
-    <Stack
-      direction="column"
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-      }}
-    >
-      <Typography variant="h4">Welcome Flex Business Solutions</Typography>
-      <Typography variant="h8">Do you want to take a note? </Typography>
+    <Grid spacing={2}>
+      <Stack
+        direction="column"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+        }}
+      >
+        <Typography variant="h4">Welcome Flex Business Solutions</Typography>
+        <Typography variant="h8">Do you want to take a note? </Typography>
 
-      <br />
-      <br />
-    </Stack>
+        <br />
+        <br />
+      </Stack>
+    </Grid>
   );
 }
